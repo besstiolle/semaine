beed0e1110e15849c72acc19aa44c601362da189
Danezis <kdanezis@gmail.com>
2020-10-05 12:32:15 +0200

    :sparkles: add basic tests

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 (function(){  
 
+    const isNddPair = (window.location.hostname == "semainepaire.fr" || window.location.hostname == "localhost" )
+    const isNddImpair = (window.location.hostname == "semainepaire.fr")
+
     let getWeekNumber = (d) => {
         // Copy date so don't modify original
         d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
@@ -14,11 +17,36 @@
         return weekNo;
     }
     
-
     let init = () => {
-            
-        console.info(getWeekNumber(new Date()));
-        console.info(window.location.hostname)
+        if(!isNddPair && !isNddImpair){
+            return;
+        }
+
+        let isPaire = ((getWeekNumber(new Date())%2) == 0);
+        let title = "";
+        let sub = "";
+
+        if(isNddPair){
+            if(isPaire){
+                title= `Yes, it's a semaine paire`
+                sub=`So you can venir à l'agence<br/>if your semaine is paire<br/>else : non, don't come à l'agence.`
+            } else {
+                title= `No, it's a semaine impaire`
+                sub=`So if your semaine is paire<br/>you can stay at your maison<br/>and come à l'agence the semaine prochaine.`
+            }
+        } else {
+            if(!isPaire){
+                title= `Yes, it's a semaine impaire`
+                sub=`So you can venir à l'agence<br/>if your semaine is impaire<br/>else : non, don't come à l'agence.`
+            } else {
+                title= `No, it's a semaine paire`
+                sub=`So if your semaine is impaire<br/>you can stay at your maison<br/>and come à l'agence the semaine prochaine.`
+            }
+        }
+
+        document.getElementById('title').innerHTML = title;
+        document.getElementById('subtitle').innerHTML = sub;
+
     } 
 
     init();
